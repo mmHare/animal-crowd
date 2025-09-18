e18d48c284f2a2049634a49570861a216573d6c8
mmHare <madmarchhare65@gmail.com>
2025-09-18 19:52:07 +0200

    Pierwsza wersja ZOO

---
+++ zoo.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "Some sound"
+    
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+    
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+    
+
+if __name__ == "__main__":
+    animals = [Dog("Burek"), Cat("Mruczek")]
+    for a in animals:
+        print(f"{a.name}: {a.speak()}")
+
